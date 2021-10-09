5a6d443ac3a18a42bf1569aadbc469863b7ded6b
thangved <thangcreation@gmail.com>
2021-10-09 17:30:33 +0700

    OK

--- src/components/Col.js
+++ src/components/Col.js
@@ -2,7 +2,6 @@ import React from 'react'
 import style from './app.css'
 
 const Col = ({ children, sm, md, lg }) => {
-  console.log(md)
   return (
     <div
       className={`${style.col} ${sm && style[`sm-${sm}`] || ''} ${md && style[`md-${md}`] || ''} ${lg && style[`lg-${lg}`] || ''}`}>
