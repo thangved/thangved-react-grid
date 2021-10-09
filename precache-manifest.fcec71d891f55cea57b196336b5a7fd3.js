75d7dbb30b171c324c5d7df5b051e7152c086027
thangved <thangcreation@gmail.com>
2021-10-09 17:30:17 +0700

    Updates

---
+++ precache-manifest.fcec71d891f55cea57b196336b5a7fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069caeafbd4d7510fea44199c16dac45",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d25a5d65001ad4e397d",
+    "url": "./static/css/main.36440bb2.chunk.css"
+  },
+  {
+    "revision": "d5dd9dfd57500245d2fb",
+    "url": "./static/js/2.cd396278.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd396278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d25a5d65001ad4e397d",
+    "url": "./static/js/main.1276a674.chunk.js"
+  },
+  {
+    "revision": "5bb54ecfc0addd3b7b20",
+    "url": "./static/js/runtime-main.a18f7762.js"
+  }
+]);
